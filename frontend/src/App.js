70c573fd2160611dc54642b1d5dcd0208178314b
Brian <brianzzl@hotmail.com>
2020-10-22 02:00:19 -0400

    basic homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
-  return <>shopbtc</>;
+  return (
+    <>
+      <Header />
+
+      <main className='py-3'>
+        <Container>
+          <h1>shopBTC shop with BTC</h1>
+        </Container>
+      </main>
+
+      <Footer />
+    </>
+  );
 };
 
 export default App;
